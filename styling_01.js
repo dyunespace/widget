2d0ee0b7d662076900e13a62298c81b67bc6a6be
dyunespace <jjung@bsgglobal.com>
2026-06-26 15:29:31 +0900

    Update styling_01.js

--- styling_01.js
+++ styling_01.js
@@ -31,7 +31,7 @@
 	<div class="row">
 		<label for="sel_defaultLevel">Default Level</label>
 		<select id="sel_defaultLevel">
-			<option value="1">0</option>
+			<option value="0">0</option>
 			<option value="1">1</option>
 			<option value="2">2</option>
 			<option value="3">3</option>
